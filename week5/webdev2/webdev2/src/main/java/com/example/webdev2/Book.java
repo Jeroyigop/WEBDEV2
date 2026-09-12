45d997d09609d28621704f496aea80ea18cc4d56
Jeroyigop <jerie.corales@lorma.edu>
2026-09-12 13:33:52 +0800

    Add files via upload

---
+++ week5/webdev2/webdev2/src/main/java/com/example/webdev2/Book.java
@@ -0,0 +1,65 @@
+package com.example.webdev2;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class Book {
+
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    @Size(min = 2, max = 100, message = "Title must be between 2 and 100 characters")
+    private String title;
+
+    @NotBlank(message = "Author is required")
+    @Size(min = 2, max = 100, message = "Author must be between 2 and 100 characters")
+    private String author;
+
+    @Min(value = 1000, message = "Publication year must be at least 1000")
+    @Max(value = 2100, message = "Publication year must not exceed 2100")
+    private int publicationYear;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String author, int publicationYear) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
